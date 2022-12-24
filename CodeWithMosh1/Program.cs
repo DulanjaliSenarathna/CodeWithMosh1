2688028c3741ec721d75fba80e23442c1a22e5f1
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-24 15:48:28 +0530

    Demo Variables and Constants

--- CodeWithMosh1/Program.cs
+++ CodeWithMosh1/Program.cs
@@ -6,7 +6,22 @@ namespace CodeWithMosh1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Dulanjali";
+            var isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MaxValue , byte.MinValue);
+            Console.WriteLine("{0} {1}", float.MaxValue , float.MinValue);
         }
     }
 }
